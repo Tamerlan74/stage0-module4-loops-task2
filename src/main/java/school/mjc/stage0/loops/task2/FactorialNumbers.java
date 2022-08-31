04a60fd96f61074f2632dcf34f9ca5293d09c32f
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-08-31 16:49:14 +0600

    do

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int i = 0;
+        while (i<=printToInclusive){
+            if (i == 0){
+                System.out.println(++i);
+            }else {
+                System.out.println(i);
+                i = i*++i;
+            }
+        }
     }
 }
